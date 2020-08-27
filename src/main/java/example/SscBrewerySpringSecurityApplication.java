877858cfe46740cd5f329f505a3cb114f453e87a
Long Nguyen <nguyenhlongvn@gmail.com>
2020-08-27 16:28:10 -0500

    Initial commit

---
+++ src/main/java/example/SscBrewerySpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SscBrewerySpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SscBrewerySpringSecurityApplication.class, args);
+	}
+
+}
